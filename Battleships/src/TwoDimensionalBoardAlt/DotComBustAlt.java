7a89ce07fe34bca4b868bfeec3025d78550616c1
YeahThatGuy <cr.josan@gmail.com>
2018-04-11 13:45:03 +0100

    Improved the code by checking in all directions and avoiding overlapping objects

--- Battleships/src/TwoDimensionalBoardAlt/DotComBustAlt.java
+++ Battleships/src/TwoDimensionalBoardAlt/DotComBustAlt.java
@@ -1,5 +1,6 @@
 //the difference between TwoDimensionalBoard.DotComBurst and this lies in the reduction of local variables
 //they've been replaced by 2x ArrayList, one for vertical coordinates, the other for horizontal
+//DotComs should not overlap, their historic locations are logged in locationsLog
 package TwoDimensionalBoardAlt;
 
 import java.util.ArrayList;
@@ -15,9 +16,11 @@ public class DotComBustAlt {
         ArrayList<String> locations = new ArrayList<>();
         ArrayList<Integer> locationsVertical = new ArrayList<>();
         ArrayList<Integer> locationsHorizontal = new ArrayList<>();
+        ArrayList<String> locationsLog = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
+        restart:
         while (numberOfDotComs > 0) {
             dot = new DotComAlt();
             System.out.print("Name the DotCom: ");
@@ -27,7 +30,7 @@ public class DotComBustAlt {
             locationsHorizontal.add((int) (Math.random() * 7));
             int directionChoice;
 
-            if (((locationsVertical.get(0) >= 0) && (locationsVertical.get(0) <= 4)) && ((locationsHorizontal.get(0) >= 0) && (locationsHorizontal.get(0) <= 4))) {
+            if ((locationsVertical.get(0) <= 4) && (locationsHorizontal.get(0) <= 4)) {
                 // 0 for down, 1 for right
                 directionChoice = (int) (Math.random() * 2);
                 if (directionChoice == 0) {
@@ -41,12 +44,43 @@ public class DotComBustAlt {
                     locationsVertical.add(locationsVertical.get(0));
                     locationsVertical.add(locationsVertical.get(0));
                 }
-            } else {
+            }
+            else if ((locationsVertical.get(0) > 4) && (locationsHorizontal.get(0) > 4)) {
                 // 0 for up, 1 for left
                 directionChoice = (int) (Math.random() * 2);
                 if (directionChoice == 0) {
                     locationsVertical.add(locationsVertical.get(0) - 1);
                     locationsVertical.add(locationsVertical.get(0) - 2);
+                    locationsHorizontal.add(locationsHorizontal.get(0));
+                    locationsHorizontal.add(locationsHorizontal.get(0));
+                } else {
+                    locationsHorizontal.add(locationsHorizontal.get(0) - 1);
+                    locationsHorizontal.add(locationsHorizontal.get(0) - 2);
+                    locationsVertical.add(locationsVertical.get(0));
+                    locationsVertical.add(locationsVertical.get(0));
+                }
+            }
+            else if ((locationsVertical.get(0) > 4) && (locationsHorizontal.get(0) <= 4)) {
+                // 0 for up, 1 for right
+                directionChoice = (int) (Math.random() * 2);
+                if (directionChoice == 0) {
+                    locationsVertical.add(locationsVertical.get(0) - 1);
+                    locationsVertical.add(locationsVertical.get(0) - 2);
+                    locationsHorizontal.add(locationsHorizontal.get(0));
+                    locationsHorizontal.add(locationsHorizontal.get(0));
+                } else {
+                    locationsHorizontal.add(locationsHorizontal.get(0) + 1);
+                    locationsHorizontal.add(locationsHorizontal.get(0) + 2);
+                    locationsVertical.add(locationsVertical.get(0));
+                    locationsVertical.add(locationsVertical.get(0));
+                }
+            }
+            else if ((locationsVertical.get(0) <= 4) && (locationsHorizontal.get(0) > 4)) {
+                // 0 for down, 1 for left
+                directionChoice = (int) (Math.random() * 2);
+                if (directionChoice == 0) {
+                    locationsVertical.add(locationsVertical.get(0) + 1);
+                    locationsVertical.add(locationsVertical.get(0) + 2);
                     locationsHorizontal.add(locationsHorizontal.get(0));
                     locationsHorizontal.add(locationsHorizontal.get(0));
                 } else {
@@ -66,12 +100,24 @@ public class DotComBustAlt {
             String secondLocation = secondLocationV + Integer.toString(locationsHorizontal.get(1));
             String thirdLocation = thirdLocationV + Integer.toString(locationsHorizontal.get(2));
 
+            //check if DotComs overlap; if yes then randomise the initial location again
+            for (String cell : locationsLog) {
+                if (cell.equals(firstLocation) || (cell.equals(secondLocation) || (cell.equals(thirdLocation)))) {
+                    continue restart;
+                }
+            }
+
             locations.add(firstLocation);
             locations.add(secondLocation);
             locations.add(thirdLocation);
             dot.setLocationCells(locations);
+
             locationsVertical.clear();
             locationsHorizontal.clear();
+
+            locationsLog.add(firstLocation);
+            locationsLog.add(secondLocation);
+            locationsLog.add(thirdLocation);
 
             boolean isAlive = true;
             while (isAlive) {
